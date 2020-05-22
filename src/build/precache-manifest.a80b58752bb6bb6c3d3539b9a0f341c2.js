e260d32ece0ea1c29e6b0fdc35d0f1911cf24316
System Administrator <root@Christians-MacBook-Pro.local>
2020-05-22 18:40:52 +0700

    Completed app

---
+++ src/build/precache-manifest.a80b58752bb6bb6c3d3539b9a0f341c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0613bb3bb2420ba7d67b30049f6575f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "851d5e5321f8a52b9590",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "7a3b41cd652bfc47cd18",
+    "url": "/static/js/2.d13be871.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d13be871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851d5e5321f8a52b9590",
+    "url": "/static/js/main.34bd8bce.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
---
+++ src/server.js
@@ -0,0 +1,134 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import MongoClient from 'mongodb';
+import { withDB } from '../withDB';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.use(bodyParser.json());
+
+app.get('/hello', (req, res) => res.send('Hello'));
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}!`);
+
+});
+
+app.post('/hello', (req, res) => {
+    const name = req.body.name;
+    res.send(`Hello Post ${name}!`);
+});
+
+
+
+app.get('/api/oldway/articles/:name', async (req, res) => {
+    const articleName = req.params.name;
+
+    try{
+        const client = await MongoClient.connect(
+            'mongodb://localhost:27017',
+            { useNewUrlParser: true, useUnifiedTopology: true},
+        );
+    
+        const db = client.db('react-blog-db');
+        const articleInfo = await db.collection('articles')
+            .findOne({name : articleName});
+        
+        client.close();
+        if(!articleInfo) return res.status(404).send('Article Not Found!');
+    
+        res.status(200).json(articleInfo);
+    } catch(e) {
+        console.error(e);
+        res.status(500).send('Something went wrong!');
+    }
+});
+
+app.get('/api/articles/:name', async (req, res) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await withDB(async db => {
+        return await db.collection('articles')
+            .findOne({name : articleName});
+    });
+
+    if(!articleInfo) return res.status(404).send('Article Not Found!');
+    
+    res.status(200).json(articleInfo);
+});
+
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    const articleName = req.params.name;
+    try{
+        const client = await MongoClient.connect(
+            'mongodb://localhost:27017',
+            { useNewUrlParser: true, useUnifiedTopology: true},
+        );
+    
+        const db = client.db('react-blog-db');
+        const articleInfo = await db.collection('articles')
+            .findOne({name : articleName});
+        if(!articleInfo) return res.status(404).send('Article Not Found!');
+    
+        await db.collection('articles')
+            .updateOne({name : articleName}, {
+                '$set': {
+                    upvotes: articleInfo.upvotes + 1,
+                }
+            });
+        
+        const updatedArticleInfo = await db.collection('articles')
+            .findOne({name : articleName});
+        
+
+        client.close();
+        
+        res.status(200).json(updatedArticleInfo);
+    } catch(e) {
+        console.error(e);
+        res.status(500).send('Something went wrong!');
+    }
+})
+
+app.post('/api/articles/:name/add-comment', async (req, res) => {
+    const articleName = req.params.name;
+    const { comment } = req.body;
+    
+    try{
+        const client = await MongoClient.connect(
+            'mongodb://localhost:27017',
+            { useNewUrlParser: true, useUnifiedTopology: true},
+        );
+    
+        const db = client.db('react-blog-db');
+        const articleInfo = await db.collection('articles')
+            .findOne({name : articleName});
+        if(!articleInfo) return res.status(404).send('Article Not Found!');
+    
+        await db.collection('articles')
+            .updateOne({name : articleName}, {
+                '$set': {
+                    comments: articleInfo.comments.concat(comment),
+                }
+            });
+        
+        const updatedArticleInfo = await db.collection('articles')
+            .findOne({name : articleName});
+        
+
+        client.close();
+        
+        res.status(200).json(updatedArticleInfo);
+    } catch(e) {
+        console.error(e);
+        res.status(500).send('Something went wrong!');
+    }
+})
+
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+});
